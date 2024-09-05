84afdf1ca7fcd6a62198d4f8b39d7be4ee1bd6af
MUTHU PANDI <97425764+muthu4600@users.noreply.github.com>
2024-09-05 15:20:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,9 @@ export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
   build: {
     assetsInlineLimit: 1024,
+    rollupOptions: {
+      external: ['nodemailer'],
+    },
   },
   server: {
     port: 7777,
